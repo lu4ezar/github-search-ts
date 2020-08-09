c663311f9e5133475e2b174009c2233d00be3a5d
lu4ezar <misha-shisha@mail.ru>
2020-08-10 00:26:43 +0700

    add selector for page

--- src/redux/selectors.ts
+++ src/redux/selectors.ts
@@ -1,3 +1,4 @@
+import { createSelector } from "@reduxjs/toolkit";
 import { RootState } from "./store";
 
 export const selectSearchString = (state: RootState) => state.searchString;
@@ -5,3 +6,15 @@ export const selectRepos = (state: RootState) => state.repos;
 export const selectLoadingStatus = (state: RootState) => state.loading;
 export const selectError = (state: RootState) => state.error;
 export const selectPagination = (state: RootState) => state.pagination;
+
+export const selectCurrentPage = createSelector(
+  [selectPagination, selectRepos],
+  (pagination, repos) => {
+    const data = repos.filter(
+      (_, index) =>
+        index > (pagination.currentPage - 1) * 10 &&
+        index < pagination.currentPage - 1 + 10
+    );
+    return data;
+  }
+);
