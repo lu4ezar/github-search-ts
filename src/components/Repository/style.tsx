c76b8f57f4ca051d7ab4847fe2add88802076150
lu4ezar <misha-shisha@mail.ru>
2020-08-09 17:21:13 +0700

    add padding

--- src/components/Repository/style.tsx
+++ src/components/Repository/style.tsx
@@ -4,6 +4,7 @@ export const StyledRepository = styled.div`
   box-sizing: border-box;
   border: 1px solid black;
   margin: 1em 0;
+  padding: 1em;
   & > p {
     color: cornflowerblue;
     overflow: hidden;
