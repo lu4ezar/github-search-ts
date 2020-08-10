14f9d77a79d3a8352fab8bcfee86f02149cb2df0
lu4ezar <misha-shisha@mail.ru>
2020-08-10 20:44:34 +0700

    repoSlice upd

--- src/redux/slices/reposSlice.ts
+++ src/redux/slices/reposSlice.ts
@@ -1,20 +1,38 @@
 import { createSlice } from "@reduxjs/toolkit";
-import { IRepoArray } from "../../types/repos";
-import { fetchSuccess } from "../fetchActions";
+import { IRepo, IRepoArray } from "../../types/repos";
+import { fetchSuccess, dropState } from "../fetchActions";
+import { AxiosResponse } from "axios";
 
 export const reposSlice = createSlice({
   name: "repos",
   initialState: [] as IRepoArray,
-  reducers: {
-    clearRepos: () => [] as IRepoArray
-  },
+  reducers: {},
   extraReducers: builder =>
-    builder.addCase(fetchSuccess, (state, action) => {
-      const {
-        payload: { repos }
-      } = action;
-      return [...state, ...repos];
-    })
+    builder
+      .addCase(fetchSuccess, (state, action) => {
+        const {
+          payload: { repos }
+        } = action;
+        const strippedRepos = repos.map(
+          ({
+            id,
+            name,
+            html_url: url,
+            stargazers_count: stargazers,
+            watchers_count: watchers,
+            forks_count: forks
+          }: AxiosResponse["data"]): IRepo => ({
+            id,
+            name,
+            url,
+            stargazers,
+            watchers,
+            forks
+          })
+        );
+        return [...state, ...strippedRepos];
+      })
+      .addCase(dropState, () => [] as IRepoArray)
 });
 
 export default reposSlice.reducer;
