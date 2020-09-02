16dca6461cdf57af4480204c11cba36ed2a40cbd
lu4ezar <misha-shisha@mail.ru>
2020-09-02 20:11:00 +0700

    update Pagination interface

--- src/types/pagination.ts
+++ src/types/pagination.ts
@@ -1,11 +1,11 @@
+import { Links } from 'parse-link-header';
+
 export type CurrentPage = number;
 export type TotalPages = number;
 export type LoadedPages = number;
 
-export interface IPagination {
-  currentPage: CurrentPage;
-  loadedPages: LoadedPages;
-  totalPages: TotalPages;
+export interface IPagination extends Links {
+  current: Links['page'];
 }
 
 export type IPaginationState = Readonly<IPagination>;
