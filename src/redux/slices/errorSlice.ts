d586f48453f6e85ea7d38f31e99ff85875d7a033
lu4ezar <misha-shisha@mail.ru>
2020-09-02 20:15:04 +0700

    format errorSlice

--- src/redux/slices/errorSlice.ts
+++ src/redux/slices/errorSlice.ts
@@ -7,11 +7,10 @@ const errorSlice = createSlice({
   name: 'error',
   initialState: null as IError,
   reducers: {},
-  extraReducers: (builder) =>
-    builder
-      .addCase(fetchFailure, (_, action) => action.payload)
-      .addCase(fetchStart, (_, __) => null)
-      .addCase(searchStringSlice.actions.setSearchString, (_, __) => null),
+  extraReducers: (builder) => builder
+    .addCase(fetchFailure, (_, action) => action.payload)
+    .addCase(fetchStart, (_, __) => null)
+    .addCase(searchStringSlice.actions.setSearchString, (_, __) => null),
 });
 
 export default errorSlice.reducer;
