a2c39aec030d136d5ad3c37828f4bd7e1e8f5df5
lu4ezar <misha-shisha@mail.ru>
2020-09-04 22:04:42 +0700

    update paginationSlice

--- src/redux/slices/paginationSlice.ts
+++ src/redux/slices/paginationSlice.ts
@@ -1,31 +1,21 @@
 import { createSlice } from '@reduxjs/toolkit';
 import { fetchSuccess, dropState } from '../fetchActions';
+import { IPagination } from '../../types/pagination';
 
-const initialState = {
-  currentPage: 0,
-  loadedPages: 0,
-  totalPages: 0,
-};
+const initialState = {} as IPagination;
 
 export const paginationSlice = createSlice({
   name: 'pagination',
   initialState,
-  reducers: {
-    setPage: (state, action) => {
-      state.currentPage = action.payload;
-    },
-  },
-  extraReducers: (builder) =>
-    builder
-      .addCase(fetchSuccess, (state, action) => {
-        const {
-          payload: { totalPages },
-        } = action;
-        state.currentPage += 1;
-        state.loadedPages += 1;
-        state.totalPages = totalPages;
-      })
-      .addCase(dropState, () => initialState),
+  reducers: {},
+  extraReducers: (builder) => builder
+    .addCase(fetchSuccess, (_, action) => {
+      const {
+        payload: { pagesInfo },
+      } = action;
+      return pagesInfo;
+    })
+    .addCase(dropState, () => initialState),
 });
 
 export default paginationSlice.reducer;
