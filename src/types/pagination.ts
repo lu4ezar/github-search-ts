d45c1aea08381dee504d862a10188add3c9c3b6e
lu4ezar <misha-shisha@mail.ru>
2020-09-05 13:52:35 +0700

    cleanup

--- src/types/pagination.ts
+++ src/types/pagination.ts
@@ -1,9 +1,5 @@
 import { Links } from 'parse-link-header';
 
-export type CurrentPage = number;
-export type TotalPages = number;
-export type LoadedPages = number;
-
 export interface IPagination extends Links {
   current: Links['page'];
 }
