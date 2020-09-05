4004ac391e05330c049f6eff56582c145a3bd90b
lu4ezar <misha-shisha@mail.ru>
2020-09-05 10:21:12 +0700

    change imports and typings (import cycle error)

--- src/redux/selectors.ts
+++ src/redux/selectors.ts
@@ -1,17 +1,15 @@
-import { createSelector } from '@reduxjs/toolkit';
-import { RootState } from './store';
+import { SearchString } from '../types/searchString';
+import { IPagination } from '../types/pagination';
+import { IError } from '../types/error';
+import { ILoading } from '../types/loading';
+import { IRepoArray } from '../types/repos';
 
-export const selectSearchString = (state: RootState) => state.searchString;
-export const selectRepos = (state: RootState) => state.repos;
-export const selectLoadingStatus = (state: RootState) => state.loading;
-export const selectError = (state: RootState) => state.error;
-export const selectPagination = (state: RootState) => state.pagination;
-
-export const selectCurrentPage = createSelector(
-  [selectPagination, selectRepos],
-  ({ currentPage }, repos) =>
-    repos.filter(
-      (_, index) =>
-        index > (currentPage - 1) * 10 - 1 && index < currentPage * 10,
-    ),
-);
+export const selectSearchString = (state: {
+  searchString: SearchString;
+}): SearchString => state.searchString;
+export const selectRepos = (state: { repos: IRepoArray }): IRepoArray => state.repos;
+export const selectLoadingStatus = (state: { loading: ILoading }): ILoading => state.loading;
+export const selectError = (state: { error: IError }): IError => state.error;
+export const selectPagination = (state: {
+  pagination: IPagination;
+}): IPagination => state.pagination;
